ce4233202f44bd74b41d2c0a3b09e1378b8405f4
Clint GC <clint.gordoncarroll@gmail.com>
2020-11-09 20:22:28 -0600

    completed project w/o stretch.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function MovieList(props) {
+  console.log(props);
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movie ? props.movie.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
-      ))}
+      )): <p>...loading</p> }
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  
 
   return (
+
     <div className="movie-card">
-      <h2>{title}</h2>
+      <Link to={`Movies/${id}`}><h2>{title}</h2></Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
